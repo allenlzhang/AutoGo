6cc10d7d1d37c34c759632556e1386ba8449f489
张磊 <>
2018-10-08 18:23:02 +0800

    1，首页UI开发

--- app/src/main/java/com/carlt/autogo/view/fragment/HomeFragment.java
+++ app/src/main/java/com/carlt/autogo/view/fragment/HomeFragment.java
@@ -1,14 +1,7 @@
 package com.carlt.autogo.view.fragment;
 
-import android.content.Intent;
-import android.widget.TextView;
-
 import com.carlt.autogo.R;
 import com.carlt.autogo.base.BaseMvpFragment;
-import com.carlt.autogo.common.dialog.CommonDialog;
-import com.carlt.autogo.view.activity.car.CarCertificationActivity;
-
-import butterknife.BindView;
 
 /**
  * Description: 首页fragment
@@ -17,8 +10,6 @@ import butterknife.BindView;
  * Date       : 2018/9/4 17:42
  */
 public class HomeFragment extends BaseMvpFragment {
-    @BindView(R.id.tv)
-    TextView tv;
 
     @Override
     public int getLayoutId() {
@@ -27,13 +18,13 @@ public class HomeFragment extends BaseMvpFragment {
 
     @Override
     protected void init() {
-        tv.setText("首页");
-        CommonDialog.createDialogNotitle(getActivity(), "你还没有进行车辆认证", "", "稍候再说", "立即认证", new CommonDialog.DialogWithTitleClick() {
-            @Override
-            public void onRightClick() {
-                startActivity(new Intent(getActivity(), CarCertificationActivity.class));
-            }
-        });
+//        tv.setText("首页");
+//        CommonDialog.createDialogNotitle(getActivity(), "你还没有进行车辆认证", "", "稍候再说", "立即认证", new CommonDialog.DialogWithTitleClick() {
+//            @Override
+//            public void onRightClick() {
+//                startActivity(new Intent(getActivity(), CarCertificationActivity.class));
+//            }
+//        });
     }
 
 
